a63b38f908cb0a5aba39fdaa89bed6d2be905f73
Cassie Musolf <cassiemusolf@gmail.com>
2017-05-12 14:19:55 -0700

    add link to project page using API.

--- src/Portfolio/wwwroot/js/scripts.js
+++ src/Portfolio/wwwroot/js/scripts.js
@@ -6,8 +6,12 @@
         url: 'https://api.github.com/users/cassandramusolf/starred',
         success: function (projects) {
             for (var i = 0; i <= 3; i++) {
-                $('.projects').append('<h4>' + projects[i].name.toUpperCase() + '</h4>' + '<p>' + projects[i].html_url + '</p>' + '<h3>' + projects[i].stargazers_count + ' star' + '</h3>');
+                $('.projects').append('<h4>' + projects[i].name.toUpperCase() + ' | ' + projects[i].stargazers_count + ' Star </h4>' +
+                '<a href="' + projects[i].html_url + '">' + 'View Project Here' + '</a>');
             }
         }
     });
 });
+
+
+//+ '<p>' + projects[i].html_url + '</p>'
