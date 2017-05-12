44afb868baa63ed02a0f8dfa7e489a7d0e66620e
Cassie Musolf <cassiemusolf@gmail.com>
2017-05-12 16:19:12 -0700

    change gradient color on contact form background.

--- src/Portfolio/wwwroot/js/scripts.js
+++ src/Portfolio/wwwroot/js/scripts.js
@@ -6,7 +6,7 @@
         url: 'https://api.github.com/users/cassandramusolf/starred',
         success: function (projects) {
             for (var i = 0; i <= 3; i++) {
-                $('.projects').append('<div class=col-md-4>' + '<h2 class="project-name">' + projects[i].name.toUpperCase() + ' | ' + projects[i].stargazers_count + ' Star </h2>' +
+                $('.projects').append('<div class=col-md-4 individual>' + '<h2 class="project-name">' + projects[i].name.toUpperCase() + ' <br> <br>'  + projects[i].stargazers_count + ' Star </h2>' + '<br>' +
                 '<a href="' + projects[i].html_url + '">' + 'View Project Here' + '</a>' + '</div>');
             }
         }
