c31a8f6e895eb0f8ee95615f4a13b33ea5ad7068
Cassie Musolf <cassiemusolf@gmail.com>
2017-05-12 16:14:15 -0700

    add styling and contact form.

--- src/Portfolio/wwwroot/js/scripts.js
+++ src/Portfolio/wwwroot/js/scripts.js
@@ -6,8 +6,8 @@
         url: 'https://api.github.com/users/cassandramusolf/starred',
         success: function (projects) {
             for (var i = 0; i <= 3; i++) {
-                $('.projects').append('<h4>' + projects[i].name.toUpperCase() + ' | ' + projects[i].stargazers_count + ' Star </h4>' +
-                '<a href="' + projects[i].html_url + '">' + 'View Project Here' + '</a>');
+                $('.projects').append('<div class=col-md-4>' + '<h2 class="project-name">' + projects[i].name.toUpperCase() + ' | ' + projects[i].stargazers_count + ' Star </h2>' +
+                '<a href="' + projects[i].html_url + '">' + 'View Project Here' + '</a>' + '</div>');
             }
         }
     });
