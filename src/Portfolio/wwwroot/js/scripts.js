a8a47a62fc5396fdb4a2ded91786f6023a7b2c4a
Cassie Musolf <cassiemusolf@gmail.com>
2017-05-12 14:00:34 -0700

    get stargazer count showing on the page.

--- src/Portfolio/wwwroot/js/scripts.js
+++ src/Portfolio/wwwroot/js/scripts.js
@@ -5,8 +5,11 @@
         data: $(this).serialize(),
         url: 'https://api.github.com/users/cassandramusolf/repos',
         success: function (projects) {
-            for (var i = 0; i < 20; i++) {
-                $('.projects').append('<h4>' + projects[i].name + '</h4>');
+            for (var i = 0; i < projects.length; i++) {
+                //$('.projects').append('<h4>' + projects[i].name + '</h4>' + '<h3>' + projects[i].stargazers_count + '</h3>');
+                if (projects[i].stargazers_count > 0) {
+                    $('.projects').append('<h1>' + projects[i].name + '</h1>');
+                }
             }
             console.log("hello");
         }
