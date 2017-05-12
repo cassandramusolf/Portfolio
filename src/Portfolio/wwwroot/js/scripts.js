3986373eb882ff151f0f9a652f7d8f7aeca0954d
Cassie Musolf <cassiemusolf@gmail.com>
2017-05-12 14:09:35 -0700

    change url request to only display repos that have been starred.

--- src/Portfolio/wwwroot/js/scripts.js
+++ src/Portfolio/wwwroot/js/scripts.js
@@ -3,15 +3,11 @@
         type: 'GET',
         dataType: 'json',
         data: $(this).serialize(),
-        url: 'https://api.github.com/users/cassandramusolf/repos',
+        url: 'https://api.github.com/users/cassandramusolf/starred',
         success: function (projects) {
-            for (var i = 0; i < projects.length; i++) {
-                //$('.projects').append('<h4>' + projects[i].name + '</h4>' + '<h3>' + projects[i].stargazers_count + '</h3>');
-                if (projects[i].stargazers_count > 0) {
-                    $('.projects').append('<h1>' + projects[i].name + '</h1>');
-                }
+            for (var i = 0; i <= 3; i++) {
+                $('.projects').append('<h4>' + projects[i].name + '</h4>' + '<h3>' + projects[i].stargazers_count + '</h3>');
             }
-            console.log("hello");
         }
     });
 });
